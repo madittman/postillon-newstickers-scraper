4d502ef56c2f6cb29be2a9db6ede2034d57c5b0d
madittman <mdtn@gmx.de>
2026-02-10 15:44:06 +0100

    Rename and update UrlsToJsonDumper

--- urls_to_json_dumper/urls_to_json_dumper.py
+++ urls_to_json_dumper/urls_to_json_dumper.py
@@ -3,18 +3,18 @@ from dataclasses import dataclass
 
 
 @dataclass
-class UrlsJsonDumper:
+class UrlsToJsonDumper:
     """Class for dumping all valid URL's from UrlBuilder to a JSON file."""
 
     # urls is a dict in the format <newsticker number, URL>
     urls: dict[int, str|None]
 
-    def _clean_urls(self) -> None:
+    def _remove_none_values(self) -> None:
         """Remove all non-existent URLs."""
         self.urls = {k: v for k, v in self.urls.items() if v is not None}
 
-    def dump_urls(self) -> None:
-        """Method to clean and dump all valid URLs to a JSON file 'urls.json'."""
-        self._clean_urls()
-        with open("urls.json", "w", encoding="utf-8") as json_file:
+    def dump_urls(self, filename: str) -> None:
+        """Method to dump all URLs to a JSON file."""
+        self._remove_none_values()
+        with open(filename, "w", encoding="utf-8") as json_file:
             json.dump(self.urls, json_file, ensure_ascii=False, indent=4)
