b235437dee63e7c54122a80cf03b63d5c1450782
madittman <mdtn@gmx.de>
2026-02-14 11:12:01 +0100

    Rename variables in abstract NewstickersParser class

--- newstickers_parsers/newstickers_parser.py
+++ newstickers_parsers/newstickers_parser.py
@@ -52,18 +52,18 @@ class NewstickersParser(ABC):
 
     def _get_title_string(self) -> str:
         """Return the title of the newstickers website as string."""
-        title: Tag | None = self.soup.title
-        if not title:
+        title_tag: Tag | None = self.soup.title
+        if not title_tag:
             raise NoValidTitleFoundError(
                 f"Title from URL '{self.url}' could not be parsed"
             )
-        title_string: str = str(title.string)
+        title_string: str = str(title_tag.string)
 
         # Raise error if title doesn't match the format 'word1-word2-...-wordN (<number>).*'
         pattern: str = r"^[\w-]+ \(\d+\).*"
         if not re.fullmatch(pattern, title_string):
             raise NoValidTitleFoundError(
-                f"Title '{title}' from URL '{self.url}' does not match format 'Newsticker (<number>) .*"
+                f"Title '{title_string}' from URL '{self.url}' does not match format 'Newsticker (<number>) .*"
             )
 
         return title_string
@@ -81,12 +81,12 @@ class NewstickersParser(ABC):
 
     def _get_date(self) -> date:
         """Return the date of the newstickers website as date object."""
-        time: Tag | None = self.soup.time
-        if not time:
+        time_tag: Tag | None = self.soup.time
+        if not time_tag:
             raise NoValidDateFoundError(
                 f"Time from URL '{self.url}' could not be parsed"
             )
-        date_string: str = str(time.string)
+        date_string: str = str(time_tag.string)
 
         # Raise error when date doesn't match the format 'day.month.year'
         pattern = r"^[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}$"
